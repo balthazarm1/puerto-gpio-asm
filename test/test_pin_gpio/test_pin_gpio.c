a67b37a5da8c9ea90eec98856792fd108a4fe4a8
Balthazar Martin <balthazarm8@gmail.com>
2022-05-25 20:34:33 -0300

    escribe salida bajo echo

--- test/test_pin_gpio/test_pin_gpio.c
+++ test/test_pin_gpio/test_pin_gpio.c
@@ -107,7 +107,7 @@ int main(void)
     RUN_TEST(configura_entrada);
     RUN_TEST(configura_salida);
     RUN_TEST(configura_salida_od);
-    //RUN_TEST(escribe_en_salida_bajo);
+    RUN_TEST(escribe_en_salida_bajo);
     //RUN_TEST(escribe_en_salida_alto);
     //RUN_TEST(configura_entrada_pullUp);
     //RUN_TEST(configura_entrada_pullDown);
